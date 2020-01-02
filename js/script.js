6b9d28bfb61fa7315dd624b64de3c963caee216b
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-01-02 20:07:19 +0100

    changeofconcept

--- js/script.js
+++ js/script.js
@@ -8,8 +8,31 @@ $(".hamburger").on("click", function(e) {
 
 // Przesunięcie po kliknięciu strzałki
 
+var main = $(".main-top");
+
 $("#arrow").on("click", function () {
     $("body, html").animate({
-        scrollTop: $(".about").offset().top
+        scrollTop: main.offset().top
     }, 1200)
+})
+
+// Zmiana tła navbaru po scrollu
+
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    var topSize = $('.header-top').height() * 1.25;
+    var h = $('header').height() - topSize;
+    var hdouble = $('header').height() * 1.75;
+    
+    if (y > h) {
+        $(".hamburger span").css("background-color", "#ffffff");
+    } else {
+        $(".hamburger span").css("background-color", "#95a3b3");
+    };
+
+    if (y > hdouble) {
+        $("nav").css("background-color", "#95a3b3");
+    } else {
+        $("nav").css("background-color", "initial");
+    }
 })
